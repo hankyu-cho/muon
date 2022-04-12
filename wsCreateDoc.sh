7db62dd32feb0923dc1fdaf2b462caf90d9b3112
hankyu-cho <93356611+hankyu-cho@users.noreply.github.com>
2022-04-12 13:58:50 +0900

    Update wsCreateDoc.sh

--- wsCreateDoc.sh
+++ wsCreateDoc.sh
@@ -1,5 +1,15 @@
+#!/bin/sh              
 
-#!/bin/sh
+ #
+ # Copyright 2021 by HK.JO    
+ #
+ #ident  "wsCreateDoc.sh         1.0    2021/04/12
+
+ #
+ # 웹스퀘어5에서 사용하는 WDocs실행 스크립트. JDK 경로 인자를 주는 경우 해당 경로의 java실행. 
+ # setenv 역활을 하며 startwsdoc.sh에서 jar화일을 실행한다.  $# -ne 의 사용법 예시
+
+
 JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/jre/bin
 if [ $# -ne 0 ]
 then
